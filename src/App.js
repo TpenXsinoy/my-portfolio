422e05b026cb7d2ef71551cf7b119c6351bc0527
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-07 02:33:22 +0800

    Add

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import React, { useEffect } from 'react'
+import 'material-icons/iconfont/material-icons.css'
+import { Button, Icon, ButtonLink, IconButton } from '../src/components';
 import './styles/App.scss';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'Stephine Sinoy'
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button icon="phone">ASDAS</Button>
+        <Icon icon='call'/>
+        <ButtonLink to="https://www.facebook.com/">asdas</ButtonLink>
+        <IconButton icon="phone"> asdasdas </IconButton>
       </header>
     </div>
   );
