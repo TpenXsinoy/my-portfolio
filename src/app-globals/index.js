a091cd26d2eeea95635bb0e2894963a6e8bd51e6
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-07 02:32:53 +0800

    Add globals

---
+++ src/app-globals/index.js
@@ -0,0 +1,17 @@
+import BUTTON_KINDS from './buttonKinds'
+import COLOR_CLASSES from './colorClasses'
+import COLOR_HEX_CODES from './colorHexCodes'
+import COLOR_NAMES from './colorNames'
+import FONT_SIZES from './fontSizes'
+import FONT_WEIGHTS from './fontWeights'
+
+const GLOBALS = {
+  BUTTON_KINDS,
+  COLOR_CLASSES,
+  COLOR_HEX_CODES,
+  COLOR_NAMES,
+  FONT_SIZES,
+  FONT_WEIGHTS,
+}
+
+export default GLOBALS
