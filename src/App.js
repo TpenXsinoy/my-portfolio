5fad6f6977c9c2f75dfe01f56cd195cdbeadb607
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-07 21:54:56 +0800

    Update components and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 import "material-icons/iconfont/material-icons.css";
-import { Button, ButtonLink, IconButton, Text, Section } from "../src/elements";
-import GLOBALS from "app-globals";
-import { textTypes } from "elements/constants";
 import "./styles/App.scss";
+import { Navbar } from "components";
 
 function App() {
   useEffect(() => {
@@ -12,19 +10,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Button icon="phone">ASDAS</Button>
-        <ButtonLink to="https://www.facebook.com/">checkout webpage</ButtonLink>
-        <IconButton icon="phone"> asdasdas </IconButton>
-        <Section title="Hello world">
-          <Text
-            colorClass={GLOBALS.COLOR_CLASSES.NEUTRAL["400"]}
-            type={textTypes.HEADING.LG}
-          >
-            hahahahaha
-          </Text>
-        </Section>
-      </header>
+      <Navbar />
     </div>
   );
 }
