7cda5857e172672c26cf13d4a6a2e44d44b7cdbb
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-10 20:42:19 +0800

    Create ProjectListsCard component

---
+++ src/components/ProjectListsCard/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+import PropTypes from "prop-types";
+
+import GLOBALS from "app-globals";
+
+import { ButtonLink, Card, Text } from "elements";
+import { buttonTypes, textTypes } from "elements/constants";
+
+import styles from "./styles.module.scss";
+
+const ProjectListsCard = ({ image, name, detail, link }) => {
+  const [isCodechumModalOpen, setIsCodechumModalOpen] = useState(false);
+  return (
+    <Card className={styles.ProjectListsCard}>
+      <img className={styles.ProjectListsCard_image} src={image} alt={name} />
+
+      <Text
+        className={styles.ProjectListsCard_title}
+        colorClass={GLOBALS.COLOR_CLASSES.GREEN["100"]}
+        type={textTypes.HEADING.SM}
+      >
+        {name}
+      </Text>
+      <Text className={styles.ProjectListsCard_detail}>{detail}</Text>
+      <ButtonLink
+        to={link ? link : ""}
+        className={styles.ProjectListsCard_button}
+        type={buttonTypes.SECONDARY.GREEN}
+        onClick={() => {
+          !link && setIsCodechumModalOpen(true);
+        }}
+      >
+        <Text type={textTypes.HEADING.XXXS}>Checkout Website</Text>
+      </ButtonLink>
+    </Card>
+  );
+};
+
+ProjectListsCard.defaultProps = {
+  link: null,
+};
+
+ProjectListsCard.propTypes = {
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  detail: PropTypes.string.isRequired,
+  link: PropTypes.string,
+};
+
+export default ProjectListsCard;
