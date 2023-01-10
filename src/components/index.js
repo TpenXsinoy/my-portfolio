7a195b9babb4ce2224b4f70d344f1eb598f2ec6c
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-11 01:20:04 +0800

    Update component index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
+export { default as CodechumModal } from "./CodechumModal";
 export { default as ContactCard } from "./ContactCard";
 export { default as ProjectListsCard } from "./ProjectListsCard";
+export { default as ResumeInfo } from "./ResumeInfo";
 export { default as SectionHeader } from "./SectionHeader";
 export { default as SkillList } from "./SkillList";
