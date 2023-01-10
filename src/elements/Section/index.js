6c7295d31ac12e9c64685fd5260e0072e7670acf
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-10 20:41:34 +0800

    Update section element

--- src/elements/Section/index.js
+++ src/elements/Section/index.js
@@ -3,68 +3,33 @@ import React from "react";
 import cn from "classnames";
 import PropTypes from "prop-types";
 
-import { useWindowSize } from "hooks";
 import { Text } from "..";
 import { textTypes } from "../constants";
 
 import styles from "./styles.module.scss";
 
-const Section = ({
-  id,
-  children,
-  className,
-  titleClassName,
-  title,
-  subInfo,
-}) => {
-  const { isSmallMobile, isMobile, isTablet, isSmallDesktop, isDesktop } =
-    useWindowSize();
-  return (
-    <section
-      className={cn(styles.Section, className)}
-      id={(() => {
-        if (isSmallDesktop || isDesktop) {
-          if (subInfo) return null;
-        }
-        return id;
-      })()}
-    >
-      {title && (
-        <div className={styles.Section_details}>
-          <div className={cn(styles.Section_title, titleClassName)}>
-            <Text
-              className={cn(styles.Section_title_text, titleClassName)}
-              id={id ? `${id}-title` : null}
-              type={(() => {
-                if (isSmallDesktop || isTablet) return textTypes.HEADING.XXL;
-                else if (isSmallMobile) return textTypes.HEADING.LG;
-                else if (isMobile) return textTypes.HEADING.XL;
-                return textTypes.HEADING.XXXL;
-              })()}
-            >
-              {title}
-            </Text>
-            <div className={styles.Section_title_line}></div>
-          </div>
-
-          {subInfo && (
-            <Text className={styles.Section_subInfo} id={id}>
-              {subInfo}
-            </Text>
-          )}
-        </div>
-      )}
-      {children}
-    </section>
-  );
-};
+const Section = ({ id, children, className, titleClassName, title }) => (
+  <section className={cn(styles.Section, className)} id={id}>
+    {title && (
+      <div className={cn(styles.Section_title, titleClassName)}>
+        <Text
+          className={cn(styles.Section_title_text, titleClassName)}
+          id={id ? `${id}-title` : null}
+          type={textTypes.HEADING.XXXL}
+        >
+          {title}
+        </Text>
+      </div>
+    )}
+    {children}
+  </section>
+);
 
 Section.defaultProps = {
   id: null,
   className: null,
   children: null,
   title: null,
-  subInfo: null,
 };
 
 Section.propTypes = {
@@ -73,7 +38,6 @@ Section.propTypes = {
   titleClassName: PropTypes.string,
   children: PropTypes.any,
   title: PropTypes.string,
-  subInfo: PropTypes.string,
 };
 
 export default Section;
