49622af05242092d25ec78448bcbedfa05c91851
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-10 20:42:32 +0800

    Implement Portfolio section

---
+++ src/sections/Portfolio/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+import GLOBALS from "app-globals";
+
+import { ProjectListsCard, SectionHeader } from "components";
+import { Container, Section } from "elements";
+
+import Xmuse from "../../static/images/projects/xmuse.png";
+import Codechum from "../../static/images/projects/codechum.png";
+import Alivee from "../../static/images/projects/alivee.png";
+import Todo from "../../static/images/projects/todolist.png";
+
+import styles from "./styles.module.scss";
+
+const Projects = [
+  {
+    image: Xmuse,
+    name: "XMuse",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint",
+    link: "",
+  },
+  {
+    image: Codechum,
+    name: "Codechum Landing Page",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint",
+    link: null,
+  },
+  {
+    image: Alivee,
+    name: "Alivee",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint",
+    link: "",
+  },
+  {
+    image: Todo,
+    name: "Todo App",
+    detail:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint",
+    link: "",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <Section className={styles.Portfolio}>
+      <Container className={styles.Portfolio_container}>
+        <SectionHeader
+          title="PORTFOLIO"
+          subInfo={GLOBALS.SUB_INFO.PORTFOLIO}
+          id="portfolio"
+        />
+        <div className={styles.Portfolio_projects}>
+          {Projects.map((project) => (
+            <ProjectListsCard
+              image={project.image}
+              name={project.name}
+              detail={project.detail}
+              link={project.link}
+              key={project.name}
+            />
+          ))}
+        </div>
+      </Container>
+    </Section>
+  );
+};
+
+export default Portfolio;
