4d082e423a7944aa80653a662bd1a6bbf16a250f
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-11 23:05:53 +0800

    Update section subinfo

--- src/app-globals/SectionSubinfo.js
+++ src/app-globals/SectionSubinfo.js
@@ -1,10 +1,10 @@
 const SUB_INFO = {
   ABOUT:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis",
+    "I am a highly motivated and dedicated individual, with a strong passion for web development. I am known for my ability to problem-solving, attention to detail, creativity, and communication. I am also a team player and can effectively manage competing priorities. Additionally, I am always looking to expand my skills and knowledge, and I am not afraid to take on new challenges.",
   RESUME:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis",
+    "I am hardworking and dedicated individual with fine analytical and problem-solving skills, always ready to take on new challenges and make a valuable contribution to a team. Keen to work with a company where I can continue to learn and grow as a web developer.",
   PORTFOLIO:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis",
+    "These are some of my personal projects to improve my web development skills and projects from my internship at codechum and freelance work ",
 };
 
 export default SUB_INFO;
