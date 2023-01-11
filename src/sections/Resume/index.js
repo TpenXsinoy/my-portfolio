9ffc98ff12b336d17c502795e428f1e8db6f28cb
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-11 23:06:24 +0800

    Add contents in resume section

--- src/sections/Resume/index.js
+++ src/sections/Resume/index.js
@@ -15,7 +15,7 @@ const Information = [
       {
         name: "STEPHINE SINOY",
         subInfo:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas  vel sint commodi repudiandae",
+          "A results-driven person with a passion for creating visually appealing and seamless user experiences. Strong attention to detail and a ability to analyze user needs to drive website design and functionality.",
         details: [
           "Dumlog, Taslisay City, Cebu",
           "+63 906 0264 692",
@@ -55,21 +55,30 @@ const Information = [
       {
         name: "COLLEGE",
         subInfo: null,
-        details: ["Bachelor of Science in Computer Science"],
+        details: [
+          "Bachelor of Science in Computer Science",
+          "TOP 10 Presidential Scholar S.Y. 2021-2022",
+          "Parangal Awardee (College Scholar 2020-2022)",
+          "Academic Scholar S.Y. 2020-2021",
+          "4th Runner Up Pitch Perfect ",
+        ],
         date: "2020 - 2023",
         org: "Cebu Institute of Technology - University",
       },
       {
         name: "SENIOR HIGH SCHOOL",
         subInfo: null,
-        details: null,
+        details: [
+          "With High Honors S.Y. 2019-2020",
+          "With Honors S.Y. 2018-2019",
+        ],
         date: "2018 - 2020",
         org: "Cebu Institute of Technology - University",
       },
       {
         name: "JUNIOR HIGH SCHOOL",
         subInfo: null,
-        details: null,
+        details: ["With Honors S.Y. 2016-2018"],
         date: "2014 - 2018",
         org: "St. Scholastica’s Academy - Tabunok",
       },
