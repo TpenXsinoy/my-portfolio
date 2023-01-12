eaafb8d26ef6d16f5fa445a610de78842d282489
iamtpensinoy <stephine.n.sinoy@gmail.com>
2023-01-13 01:23:10 +0800

    Add animation to Contact section

--- src/sections/Contact/index.js
+++ src/sections/Contact/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import cn from "classnames";
+
+import { useInView } from "react-intersection-observer";
 import { Container, Section } from "elements";
+import { ContactCard, SectionHeader } from "components";
 
 import styles from "./styles.module.scss";
-import { ContactCard, SectionHeader } from "components";
 
 const ContactInfo = [
   {
@@ -32,23 +35,33 @@ const ContactInfo = [
   },
 ];
 
-const Contact = () => (
-  <Section className={styles.Contact} id="contact">
-    <Container className={styles.Contact_container}>
-      <SectionHeader title="CONTACT" id="contact" />
-      <div className={styles.Contact_details}>
-        {ContactInfo.map((info) => (
-          <ContactCard
-            icon={info.icon}
-            name={info.name}
-            detail={info.detail}
-            hasSocials={info.hasSocials}
-            key={info.name}
-          />
-        ))}
-      </div>
-    </Container>
-  </Section>
-);
+const Contact = () => {
+  const { ref: contactRef, inView: isContactVisible } = useInView({
+    triggerOnce: true,
+  });
+  return (
+    <Section className={styles.Contact} id="contact">
+      <Container className={styles.Contact_container} ref={contactRef}>
+        <SectionHeader title="CONTACT" id="contact" />
+        <div
+          className={cn(
+            styles.Contact_details,
+            isContactVisible && styles.Contact___animate
+          )}
+        >
+          {ContactInfo.map((info) => (
+            <ContactCard
+              icon={info.icon}
+              name={info.name}
+              detail={info.detail}
+              hasSocials={info.hasSocials}
+              key={info.name}
+            />
+          ))}
+        </div>
+      </Container>
+    </Section>
+  );
+};
 
 export default Contact;
